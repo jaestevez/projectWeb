de63d4b8f00ff99db8883a0ff395e94f717338a7
Jose Estevez <jose_estevez@outlook.com>
2015-10-31 17:35:40 -0500

    beta 0.23

--- index.js
+++ index.js
@@ -12,9 +12,22 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
+app.get('/descripcion', function(request, response) {
+  response.render('pages/descripcion');
+});
+app.get('/videos', function(request, response) {
+  response.render('pages/videos');
+});
+app.get('/actividades', function(request, response) {
+  response.render('pages/actividades');
+});
+app.get('/documentos', function(request, response) {
+  response.render('pages/documentos');
+});
+app.get('/enlaces', function(request, response) {
+  response.render('pages/enlaces');
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
